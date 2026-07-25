8f96e8e3b954bea3b9c10a9280d62c0375b23748
Mohammed Ashraf <152704078+Mohammed-Zwam@users.noreply.github.com>
2026-07-26 02:44:12 +0300

    fix: route handler in SSR env ;)

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    // TODO: ASK mentor if this is the right path
     path: 'projects/:id/edit',
     renderMode: RenderMode.Server,
   },
